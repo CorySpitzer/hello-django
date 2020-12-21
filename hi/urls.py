e00502e1e9cc14faed946b2c1af6370ab0d699ca
Cory Spitzer <coryspitzer@gmail.com>
2020-12-21 10:56:33 -0500

    Add new year checker

--- hi/urls.py
+++ hi/urls.py
@@ -3,6 +3,7 @@ from . import views #. is same dir
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("cory", views.cory, name="cory")
+    path("cory", views.cory, name="cory"),
+    path("<str:name>", views.greet, name="greet")
 
 ]
--- hi/views.py
+++ hi/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hi, world!")
+    return render(request, "hi/index.html")
+    # return HttpResponse("Hi, world!")
 
 def cory(request):
     return HttpResponse("Hello, Cory!")
+
+def greet(request, name):
+    return render(request, "hi/greet.html", {
+        "name": name.capitalize()
+    })
+    # return HttpResponse(f"Hello, {name}!")
